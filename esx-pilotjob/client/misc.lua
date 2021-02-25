dc38931d02d48feb5bc25d1c7a72ec25f7081614
CommunityDevelopment <78282590+CommunityScripts@users.noreply.github.com>
2021-02-25 01:34:14 +0100

    Add files via upload

---
+++ esx-pilotjob/client/misc.lua
@@ -0,0 +1,9 @@
+--Job Script edited by 'bypass#4423'
+--Community Development https://discord.gg/gc3KHcX8Wk
+function cleanPlayer(playerPed)
+	SetPedArmour(playerPed, 0)
+	ClearPedBloodDamage(playerPed)
+	ResetPedVisibleDamage(playerPed)
+	ClearPedLastWeaponDamage(playerPed)
+	ResetPedMovementClipset(playerPed, 0)
+end
---
+++ esx-pilotjob/server/main.lua
@@ -0,0 +1,4 @@
+--Job Script edited by 'bypass#4423'
+--Community Development https://discord.gg/gc3KHcX8Wk
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
